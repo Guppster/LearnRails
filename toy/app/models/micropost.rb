36ef42db6a6bed1cb2823093aecdab272e9a2c07
Gurpreet Singh <gsingh@ca.ibm.com>
2017-03-08 23:11:18 -0500

    Updated models, Finished toy project

--- toy/app/models/micropost.rb
+++ toy/app/models/micropost.rb
@@ -1,5 +1,9 @@
 class Micropost
   include Mongoid::Document
+  belongs_to :user
+  validates :content, length: {maximum: 140}
+                    presence: true
+
   field :content, type: String
   field :user_id, type: Integer
 end
--- toy/app/models/user.rb
+++ toy/app/models/user.rb
@@ -1,5 +1,9 @@
 class User
   include Mongoid::Document
+  has_many :microposts
+  validates :name, presence: true
+  validates :email, presence: true
+
   field :name, type: String
   field :email, type: String
 end
